e2846aaab139dcc934f6f2934013ef97061f52c6
Lan Thien <lan.thiendiep@gmail.com>
2024-02-16 22:39:24 +0700

    update db path url

--- services/database_service.ts
+++ services/database_service.ts
@@ -1,24 +1,25 @@
-import mongoose, { Schema, Model, PopulateOption } from 'mongoose';
-import MemberModel, { MemberModelType } from '../models/member_model.ts'
-import LoginModel, { LoginModelType } from '../models/login_model';
-import ChurchModel from '../models/church_model.ts';
-import DepartmentModel from '../models/department_model.ts';
-import PositionModel from '../models/position_model.ts';
-import TitleModel from '../models/title_model.ts';
-var dbName = 'quan_ly_nhan_su_db'
-var url = 'mongodb://localhost:27017/'
+import mongoose, { Schema, Model, PopulateOption } from "mongoose";
+import MemberModel, { MemberModelType } from "../models/member_model.ts";
+import LoginModel, { LoginModelType } from "../models/login_model";
+import ChurchModel from "../models/church_model.ts";
+import DepartmentModel from "../models/department_model.ts";
+import PositionModel from "../models/position_model.ts";
+import TitleModel from "../models/title_model.ts";
+var dbName = "quan_ly_nhan_su_db";
+var url =
+  "mongodb+srv://lanthiendiep:HoiThanhHocMon%402024@serverlessinstance0.mx6otzy.mongodb.net/?retryWrites=true&w=majority";
 
 export default class DatabaseService {
-    constructor(){}
+  constructor() {}
 
-    async initialDatabase() {
-        const db = mongoose.connection;
-        mongoose.connect('mongodb://127.0.0.1:27017/test')
-        await LoginModel.createCollection()
-        await MemberModel.createCollection()
-        await ChurchModel.createCollection()
-        await DepartmentModel.createCollection()
-        await PositionModel.createCollection()
-        await TitleModel.createCollection()
-    }
+  async initialDatabase() {
+    const db = mongoose.connection;
+    mongoose.connect(url);
+    await LoginModel.createCollection();
+    await MemberModel.createCollection();
+    await ChurchModel.createCollection();
+    await DepartmentModel.createCollection();
+    await PositionModel.createCollection();
+    await TitleModel.createCollection();
+  }
 }
